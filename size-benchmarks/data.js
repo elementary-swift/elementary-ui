98b50209ebc75a391e1494266d783fc8bdf8c124
github-action-benchmark <github@users.noreply.github.com>
2026-05-22 08:56:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1d54665f6d465ec3061f6a15fdcf0d82d29358e2

--- size-benchmarks/data.js
+++ size-benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779355913257,
+  "lastUpdate": 1779440186516,
   "repoUrl": "https://github.com/elementary-swift/elementary-ui",
   "entries": {
     "Benchmark": [
@@ -5119,6 +5119,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/elementary-swift/elementary-ui/commit/1d54665f6d465ec3061f6a15fdcf0d82d29358e2"
         },
         "date": 1779355912684,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Animations",
+            "value": 189.26,
+            "unit": "kB"
+          },
+          {
+            "name": "Counter",
+            "value": 160.47,
+            "unit": "kB"
+          },
+          {
+            "name": "HelloWorld",
+            "value": 143.26,
+            "unit": "kB"
+          },
+          {
+            "name": "Inputs",
+            "value": 200.12,
+            "unit": "kB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Simon Leeb",
+            "username": "sliemeobn",
+            "email": "52261246+sliemeobn@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "1d54665f6d465ec3061f6a15fdcf0d82d29358e2",
+          "message": "upgraded to JavaScriptKit 0.53 (#105)\n\n* JavaScriptKit upgrade + bundled runtime\n\n* cleaned up old work arounds",
+          "timestamp": "2026-05-19T18:13:54Z",
+          "url": "https://github.com/elementary-swift/elementary-ui/commit/1d54665f6d465ec3061f6a15fdcf0d82d29358e2"
+        },
+        "date": 1779440186193,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
