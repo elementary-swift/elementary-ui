e9bf7c46092e9f2c08e3887c077e7f3c2358e8b8
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 22:22:37 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0c697d82e150c34282e45ece15fcdb519f9e8d13

--- perf-benchmarks/data.js
+++ perf-benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787435704320,
+  "lastUpdate": 1787437357837,
   "repoUrl": "https://github.com/elementary-swift/elementary-ui",
   "entries": {
     "Benchmark": [
@@ -3296,6 +3296,85 @@ window.BENCHMARK_DATA = {
           {
             "name": "09_clear1k_x8",
             "value": 61.81,
+            "unit": "ms"
+          },
+          {
+            "name": "21_ready-memory",
+            "value": 1.08,
+            "unit": "MB"
+          },
+          {
+            "name": "22_run-memory",
+            "value": 2.11,
+            "unit": "MB"
+          },
+          {
+            "name": "25_run-clear-memory",
+            "value": 1.39,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "52261246+sliemeobn@users.noreply.github.com",
+            "name": "Simon Leeb",
+            "username": "sliemeobn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c697d82e150c34282e45ece15fcdb519f9e8d13",
+          "message": "added 6.4 snapshot CI (#124)",
+          "timestamp": "2026-08-23T00:17:46+02:00",
+          "tree_id": "3a01aa719f86b045dcabbd080948c3f8b194fad4",
+          "url": "https://github.com/elementary-swift/elementary-ui/commit/0c697d82e150c34282e45ece15fcdb519f9e8d13"
+        },
+        "date": 1787437356688,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "01_run1k",
+            "value": 88.61,
+            "unit": "ms"
+          },
+          {
+            "name": "02_replace1k",
+            "value": 113.65,
+            "unit": "ms"
+          },
+          {
+            "name": "03_update10th1k_x16",
+            "value": 198.11,
+            "unit": "ms"
+          },
+          {
+            "name": "04_select1k",
+            "value": 25.25,
+            "unit": "ms"
+          },
+          {
+            "name": "05_swap1k",
+            "value": 64.71,
+            "unit": "ms"
+          },
+          {
+            "name": "06_remove-one-1k",
+            "value": 39.98,
+            "unit": "ms"
+          },
+          {
+            "name": "08_create1k-after1k_x2",
+            "value": 136.23,
+            "unit": "ms"
+          },
+          {
+            "name": "09_clear1k_x8",
+            "value": 55.21,
             "unit": "ms"
           },
           {
