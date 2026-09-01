00396e7b90c7cc652a6158424cef16e641f14cc1
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-09-01 13:03:51 +0200

    cut back on AI slop test surface

---
+++ Tests/ElementaryUITests/Reconciler/SVGModifierTests.swift
@@ -0,0 +1,62 @@
+import ElementaryUI
+import Reactivity
+import Testing
+
+struct SVGModifierTests {
+    @Test
+    func setsEventListenersOnSVGElements() {
+        let dom = TestDOM()
+        dom.mount {
+            SVG.svg {
+                SVG.rect(.x(1), .y(2), .width(10), .height(11)).onClick { _ in }
+                SVG.circle(.cx(4), .cy(5), .r(6)).onClick {}
+            }
+        }
+        dom.runNextFrame()
+
+        #expect(dom.ops.contains(.addListener(node: "<rect>", event: "click")))
+        #expect(dom.ops.contains(.addListener(node: "<circle>", event: "click")))
+        #expect(dom.eventSinkKinds == [.event, .action])
+    }
+
+    @Test
+    func setsStylesOnSVGElements() {
+        let dom = TestDOM()
+        dom.mount {
+            SVG.svg {
+                SVG.rect(.x(0), .y(0), .width(1), .height(1))
+                    .opacity(0.5)
+                    .offset(x: 10, y: 20)
+                    .blur(radius: 3)
+            }
+        }
+        dom.runNextFrame()
+
+        #expect(dom.ops.contains(.setStyle(node: "<rect>", name: "opacity", value: "0.5")))
+        #expect(dom.ops.contains(.setStyle(node: "<rect>", name: "transform", value: "translate(10.0px, 20.0px)")))
+        #expect(dom.ops.contains(.setStyle(node: "<rect>", name: "filter", value: "blur(3.0px)")))
+    }
+
+    @Test
+    func patchesStylesOnSVGElements() {
+        let state = SVGStyleToggleState()
+        let ops = patchOps {
+            SVG.svg {
+                SVG.rect(.x(0), .y(0), .width(1), .height(1)).opacity(state.value ? 1 : 0.5)
+            }
+        } toggle: {
+            state.toggle()
+        }
+
+        #expect(ops == [.setStyle(node: "<rect>", name: "opacity", value: "1.0")])
+    }
+}
+
+@Reactive
+private final class SVGStyleToggleState {
+    var value = false
+
+    func toggle() {
+        value.toggle()
+    }
+}
